633bfff7fc682d9174273c248925387b8bbb8208
robertrubin <53097504+robertrubin@users.noreply.github.com>
2020-05-28 20:19:22 -0700

    Update Minimal_Install.sh

--- Minimal_Install.sh
+++ Minimal_Install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt remove libreoffice* lightsoff quadrapassel rhythmbox shotwell swell-foop tali gnome-todo gnome-weather xterm evince totem gnome-sushi gnome-tetravex gnome-sudoku gnome-taquin gnome-robots gnome-nibbles gnome-music gnome-maps gnome-mahjongg gnome-mines gnome-klotski gnome-games gnome-documents gnome-calendar gnome-chess evolution
+sudo apt remove libreoffice* lightsoff quadrapassel rhythmbox shotwell swell-foop tali gnome-todo gnome-weather xterm evince totem gnome-sushi gnome-tetravex gnome-sudoku gnome-taquin gnome-robots gnome-nibbles gnome-music gnome-maps gnome-mahjongg gnome-mines gnome-klotski gnome-games gnome-documents gnome-calendar gnome-chess evolution gnome-sound-recorder cheese simple-scan gnome-contacts
