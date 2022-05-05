85ae5012fcc726f4dff96b7054dc4bbb820b805b
robertrubin <53097504+robertrubin@users.noreply.github.com>
2022-05-05 08:16:09 -0700

    Update Minimal_Install.sh

--- Minimal_Install.sh
+++ Minimal_Install.sh
@@ -1,3 +1,39 @@
 #!/bin/bash
 
-sudo apt remove libreoffice* lightsoff quadrapassel rhythmbox shotwell swell-foop tali gnome-todo gnome-weather xterm evince totem gnome-sushi gnome-tetravex gnome-sudoku gnome-taquin gnome-robots gnome-nibbles gnome-music gnome-maps gnome-mahjongg gnome-mines gnome-klotski gnome-games gnome-documents gnome-calendar gnome-chess evolution gnome-sound-recorder cheese simple-scan gnome-contacts five-or-more gnome-2048 four-in-a-row aisleriot hitori 
+sudo apt remove libreoffice* \
+lightsoff \
+quadrapassel \
+rhythmbox \
+shotwell \
+swell-foop \
+tali \
+gnome-todo \
+gnome-weather \
+xterm \
+evince \
+totem \
+gnome-sushi \
+gnome-tetravex \
+gnome-sudoku \
+gnome-taquin \
+gnome-robots \
+gnome-nibbles \
+gnome-music \
+gnome-maps \
+gnome-mahjongg \
+gnome-mines \
+gnome-klotski \
+gnome-games \
+gnome-documents \
+gnome-calendar \
+gnome-chess \
+evolution \
+gnome-sound-recorder \
+cheese \
+simple-scan \
+gnome-contacts \
+five-or-more \
+gnome-2048 \
+four-in-a-row \
+aisleriot \
+hitori 
